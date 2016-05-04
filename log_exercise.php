8e523b1bff54b422458cc316fdfbbd204a6f7254
Christian Manalansan <christian.manalansan@cgifederal.com>
2016-05-04 05:07:04 -0400

    Add stubs for the child pages

---
+++ log_exercise.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>Log Exercise</title>
+  </head>
+
+  <body>
+    <h1>Log Exercise</h1>
+  </body>
+</html>
---
+++ set_goal.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>Set a Fitness Goal</title>
+  </head>
+
+  <body>
+    <h1>Set a Fitness Goal</h1>
+  </body>
+</html>
