143f9987e3e5c30b065c853392dd623e7b38c182
Christian Manalansan <christian.manalansan@cgifederal.com>
2016-05-04 04:53:55 -0400

    Test for pages to exist when clicking on navigation

--- tests/Sprint1Test.php
+++ tests/Sprint1Test.php
@@ -13,7 +13,7 @@ class Sprint1Test extends PHPUnit_Framework_TestCase {
     $driver = RemoteWebDriver::create($host, $browser);
 
     // When: we go to the home page
-    $driver->get('http://ec2-54-174-113-69.compute-1.amazonaws.com'); // go to the placeholder page
+    $driver->get('http://ec2-54-174-113-69.compute-1.amazonaws.com'); // go to the home page
 
     // Then: test for a navigation menu of various options
     $actual_matches = $driver->findElements(WebDriverBy::cssSelector('ul.navigation'));
@@ -23,5 +23,14 @@ class Sprint1Test extends PHPUnit_Framework_TestCase {
     $this->assertFalse($is_found_bamboo, 'We were supposed to have removed the placeholder text for "Bamboo" yet we have not');
     $is_found_github = strpos(strtolower($driver->getPageSource()), 'github');
     $this->assertFalse($is_found_github, 'We were supposed to have removed the placeholder text for "GitHub" yet we have not');
+
+    foreach ($actual_matches as $idx => $nav_item) {
+      // When: we click on a navigational item
+      $nav_item->click();
+      // Then: test that a page actually exists
+      $this->assertNotContains('404', $driver->getTitle(), "A page was supposed to exist at navigational link #" . $idx + 1 . " but it did not.");
+      // Then: return to the home page (by URL)
+      $driver->get('http://ec2-54-174-113-69.compute-1.amazonaws.com');
+    }
   }
 }
