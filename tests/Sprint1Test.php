415cb3235c50064bdbd0edcd16fbb98ff5eb16ff
Christian Manalansan <christian.manalansan@cgifederal.com>
2016-05-04 05:05:12 -0400

    Test was flawed. We expected it to fail but it did not.

--- tests/Sprint1Test.php
+++ tests/Sprint1Test.php
@@ -18,13 +18,17 @@ class Sprint1Test extends PHPUnit_Framework_TestCase {
     // Then: test for a navigation menu of various options
     $actual_matches = $driver->findElements(WebDriverBy::cssSelector('ul.navigation'));
     $this->assertNotEquals(0, count($actual_matches), 'The home page is expected to have a nav menu, yet it does not');
+
     // Then: test that we have removed the placeholder text 'Bamboo'
     $is_found_bamboo = strpos(strtolower($driver->getPageSource()), 'bamboo');
     $this->assertFalse($is_found_bamboo, 'We were supposed to have removed the placeholder text for "Bamboo" yet we have not');
     $is_found_github = strpos(strtolower($driver->getPageSource()), 'github');
     $this->assertFalse($is_found_github, 'We were supposed to have removed the placeholder text for "GitHub" yet we have not');
 
-    foreach ($actual_matches as $idx => $nav_item) {
+    // Then: test that the navigational menu has clickable links
+    $nav_items = $driver->findElements(WebDriverBy::cssSelector('ul.navigation li a'));
+    $this->assertNotEquals(0, count($nav_items), 'The navigation is expected to have clickable items, it does not');
+    foreach ($nav_items as $idx => $nav_item) {
       // When: we click on a navigational item
       $nav_item->click();
       // Then: test that a page actually exists
